5be6fb9799478b666984fd77467dc121f1aee504
Troy Roethig <troethig@prizelogic.net>
2017-07-27 12:59:36 -0700

    updating app to load images

--- beyonce/app/App.js
+++ beyonce/app/App.js
@@ -36,18 +36,18 @@ const Container = (props) => <div>
 </div>
 
 const PhotoGallery = React.createClass({
-    
+
     render: function() {
-        
+
         var photos = this.props.photos.map(function(photo, i) {
             return (
                         <div className='col-xs-6 col-sm-4' key={i}>
                             <Photo id={photo.id} src={photo.url} key={i} caption={photo.caption} />
                         </div>
-                    
+
                    )
         });
-        
+
         return (
             <div className='photo-gallery container'>
                 {photos}
@@ -57,15 +57,15 @@ const PhotoGallery = React.createClass({
 });
 
 const Photo = React.createClass({
-    
-    
+
+
     render: function() {
         return (
             <div className='photo'>
                 <img className='img-responsive' id={this.props.id} src={this.props.src} />
                 <span>{this.props.caption}</span>
             </div>
-        ) 
+        )
     }
 });
 
@@ -73,109 +73,109 @@ const data = [
     {
         id: 'img1',
         key: 1,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img1.jpg'),
         caption: 'Image One'
     },
     {
         id: 'img2',
         key: 2,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img2.jpg'),
         caption: 'Image Two'
     },
     {
         id: 'img3',
         key: 3,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img3.jpg'),
         caption: 'Image Three'
     },
     {
         id: 'img4',
         key: 4,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img4.jpg'),
         caption: 'Image Four'
     },
     {
         id: 'img5',
         key: 5,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img5.jpg'),
         caption: 'Image Five'
     },
     {
         id: 'img6',
         key: 6,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img6.jpg'),
         caption: 'Image Six'
     },
     {
         id: 'img7',
         key: 7,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img7.jpg'),
         caption: 'Image Seven'
     },
     {
         id: 'img8',
         key: 8,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img8.jpg'),
         caption: 'Image Eight'
     },
     {
         id: 'img9',
         key: 9,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img9.jpg'),
         caption: 'Image Nine'
     },
     {
         id: 'img10',
         key: 10,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img10.jpg'),
         caption: 'Image Ten'
     },
     {
         id: 'img11',
         key: 11,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img11.jpg'),
         caption: 'Image Eleven'
     },
     {
         id: 'img12',
         key: 12,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img12.jpg'),
         caption: 'Image Twelve'
     },
     {
         id: 'img13',
         key: 13,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img13.jpg'),
         caption: 'Image Thirteen'
     },
     {
         id: 'img14',
         key: 14,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img14.jpg'),
         caption: 'Image Fourteen'
     },
     {
         id: 'img15',
         key: 15,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img15.jpg'),
         caption: 'Image Fifteen'
     },
     {
         id: 'img16',
         key: 16,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img16.jpg'),
         caption: 'Image Sixteen'
     },
     {
         id: 'img17',
         key: 17,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img17.jpg'),
         caption: 'Image Seventeen'
     },
     {
         id: 'img18',
         key: 17,
-        url: 'http://placekitten.com/800/300',
+        url: require('../assets/img18.jpg'),
         caption: 'Image Eighteen'
     }
 ]
@@ -189,49 +189,29 @@ const Address = (props) => <div>
       {props.children}
 </div>
 const AboutMe = (props) => <div>
-      <br />
-      <Link to='aboutMe/contact'>Contact Us</Link>&nbsp;
-      <Link to='aboutMe/portfolio'>Portfolio</Link>
-      <div className='col-sm-8 col-sm-offset-2'>
+      <div className='col-xs-12'>
           <div className='row'>
-            <div className='col-xs-12 col-sm-4'>
-                <img className='img-responsive'src='http://www.photl.com/images/photos/2011/11/11/1637/wm381826tt.jpg' />
-            </div>
-            <div className='col-xs-12 col-sm-8'>
-                <h1>My Name is blah blah blah</h1>
-                <h3>I like to do things with stuff</h3>
-                <p>
-                    This is a list of stuff I like to do: <br/>
-                    blah blah and I also blah blah when I like to blah<br />
-                    Blah Blah Blah and I am really good at blah blah blah!
-                </p>
+            <div className='col-xs-12'>
+                <img className='img-responsive'src={require('../assets/mainBG.jpg')} />
             </div>
           </div>
-          <div className='row'>
-              <div className='col-xs-12 col-sm-8 col-sm-offset-2'>
-                  <form>
-                    <input type='text' />&nbsp;&nbsp;
-                    <button id='submitBtn' className='btn btn-primary btn-submit'>SEND</button>
-                  </form>
-              </div>
-            </div>
       </div>
-      <div className='container col-xs-12 col-sm-8 col-sm-offset-3'>
+      <div className='container col-xs-12'>
           <Portfolio />
       </div>
 </div>
-                    
+
 const ContactUs = () => <div>
       <h1>Contact Us</h1>
 </div>
-      
+
 const Portfolio = () => <div>
       <h1>Portfolio</h1>
       <div className='container'>
           <PhotoGallery photos={data} />
       </div>
     </div>
-      
+
 
 const NotFound = () => <h1>404.. This page is not found!</h1>
 const Instagram = () => <h3>Instagram Feed</h3>
--- beyonce/webpack.config.js
+++ beyonce/webpack.config.js
@@ -16,9 +16,13 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel'
+                loader: 'babel-loader'
+            },
+            {
+              test: /\.(png|jpg)$/,
+              loader: 'url-loader?limit=8192'
             }
-            
+
         ]
     }
 }
