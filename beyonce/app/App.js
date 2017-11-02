994f363147ea887d62c1e56026b2b441d0b057e9
PRIZELOGIC\troethig <troethig@prizelogic.com>
2017-11-02 15:45:20 -0700

    updating popup gallery

--- beyonce/app/App.js
+++ beyonce/app/App.js
@@ -46,7 +46,7 @@ const PhotoGallery = React.createClass({
         var photos = this.props.photos.map(function(photo, i) {
             return (
                         <div className='col-xs-12 col-sm-3 col-md-6 col-lg-4' key={i}>
-                            <Photo id={photo.id} src={photo.url} key={i} caption={photo.caption} />
+                            <Photo id={photo.id} src={photo.url} key={i} caption={photo.caption} title={photo.title} />
                         </div>
 
 
@@ -61,19 +61,76 @@ const PhotoGallery = React.createClass({
     }
 });
 
-const Photo = React.createClass({
 
 
+class Photo extends Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
 
-    render: function() {
-        return (
-            <div className="photo">
-                <img className='img-responsive' id={this.props.id} src={this.props.src} />
-                <span>{this.props.caption}</span>
-            </div>
-        )
-    }
-});
+  handleChange(e) {
+    e.preventDefault()
+  }
+
+  handleClick(e) {
+    e.preventDefault()
+    $('.container .title').text(this.props.title);
+    $('.container .img-box img').attr("src", this.props.src);
+    $('.container .caption').text(this.props.caption);
+    $('.img-popup-wrap').removeClass('hide');
+    console.log("clicked" + this.props.id);
+  }
+  render() {
+    return (
+      <div className="photo">
+        <img className='img-responsive' onClick={this.handleClick} id={this.props.id} src={this.props.src} alt={this.props.caption} />
+        <span>{this.props.caption}</span>
+      </div>
+    )
+  }
+}
+
+
+class PopupBox extends Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleChange(e) {
+    e.preventDefault()
+  }
+
+  handleClick(e) {
+    e.preventDefault()
+    $('.img-popup-wrap').addClass('hide');
+  }
+
+  render() {
+    return (
+      <div className="container img-popup-wrap hide">
+        <div className="img-popup">
+          <div className="title">
+            Sample Title will change later
+          </div>
+          <div className="img-box">
+            <img className="img-responsive" src="" />
+          </div>
+          <div className="caption">
+            This will Contain the caption for said image.
+          </div>
+          <span className="close-btn" onClick={this.handleClick}>X</span>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+
 
 const Gallery = React.createClass ({
 
@@ -469,108 +526,126 @@ const Gallery = React.createClass ({
 
 const data = [
     {
+        title: 'Title One',
         id: 'img1',
         key: 1,
         url: require('../assets/img1.jpg'),
         caption: 'Image One'
     },
     {
+        title: 'Title Two',
         id: 'img2',
         key: 2,
         url: require('../assets/img2.jpg'),
         caption: 'Image Two'
     },
     {
+        title: 'Title Three',
         id: 'img3',
         key: 3,
         url: require('../assets/img3.jpg'),
         caption: 'Image Three'
     },
     {
+        title: 'Title Four',
         id: 'img4',
         key: 4,
         url: require('../assets/img4.jpg'),
         caption: 'Image Four'
     },
     {
+        title: 'Title Five',
         id: 'img5',
         key: 5,
         url: require('../assets/img5.jpg'),
         caption: 'Image Five'
     },
     {
+        title: 'Title Six',
         id: 'img6',
         key: 6,
         url: require('../assets/img6.jpg'),
         caption: 'Image Six'
     },
     {
+        title: 'Title Seven',
         id: 'img7',
         key: 7,
         url: require('../assets/img7.jpg'),
         caption: 'Image Seven'
     },
     {
+        title: 'Title Eight',
         id: 'img8',
         key: 8,
         url: require('../assets/img8.jpg'),
         caption: 'Image Eight'
     },
     {
+        title: 'Title Nine',
         id: 'img9',
         key: 9,
         url: require('../assets/img9.jpg'),
         caption: 'Image Nine'
     },
     {
+        title: 'Title Ten',
         id: 'img10',
         key: 10,
         url: require('../assets/img10.jpg'),
         caption: 'Image Ten'
     },
     {
+        title: 'Title Eleven',
         id: 'img11',
         key: 11,
         url: require('../assets/img11.jpg'),
         caption: 'Image Eleven'
     },
     {
+        title: 'Title Twelve',
         id: 'img12',
         key: 12,
         url: require('../assets/img12.jpg'),
         caption: 'Image Twelve'
     },
     {
+        title: 'Title Thirteen',
         id: 'img13',
         key: 13,
         url: require('../assets/img13.jpg'),
         caption: 'Image Thirteen'
     },
     {
+        title: 'Title Fourteen',
         id: 'img14',
         key: 14,
         url: require('../assets/img14.jpg'),
         caption: 'Image Fourteen'
     },
     {
+        title: 'Title Fifteen',
         id: 'img15',
         key: 15,
         url: require('../assets/img15.jpg'),
         caption: 'Image Fifteen'
     },
     {
+        title: 'Title Sixteen',
         id: 'img16',
         key: 16,
         url: require('../assets/img16.jpg'),
         caption: 'Image Sixteen'
     },
     {
+        title: 'Title Seventeen',
         id: 'img17',
         key: 17,
         url: require('../assets/img17.jpg'),
         caption: 'Image Seventeen'
     },
     {
+        title: 'Title Eighteen',
         id: 'img18',
         key: 17,
         url: require('../assets/img18.jpg'),
@@ -590,7 +665,7 @@ const AboutUs = (props) => <div>
       <div className='col-xs-12'>
           <div className='row'>
             <div className='col-xs-12'>
-                <img className='img-responsive'src={require('../assets/mainBG.jpg')} />
+
             </div>
           </div>
       </div>
@@ -600,7 +675,7 @@ const AboutUs = (props) => <div>
 </div>
 
 const ContactUs = () => <div>
-      <h1>Contact Us</h1>
+      <h1>Image Gallery</h1>
       <Gallery />
 </div>
 
@@ -610,7 +685,11 @@ const Portfolio = () => <div>
           <PhotoGallery photos={data} />
       </div>
       <Lightbox />
+      <PopupBox />
 </div>
+
+
+
 
 
 const NotFound = () => <h1>404.. This page is not found!</h1>
